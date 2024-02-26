274062d2bd53cbcf5354807cf5372a1bf062ce96
Viral-Barodia <202001007@daiict.ac.in>
2024-02-26 23:01:53 +0530

    Added options bar and persistence on DOM

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const newTodo = document.querySelector('.add-todo');
 const todos = document.querySelector('.todo-list');
 const itemInput = document.getElementById('create-todo');
-const todoList = document.querySelector('.todo-list');
+const optionsBar = document.querySelector('.options');
+const allbtn = document.querySelector('.allbtn');
+const activebtn = document.querySelector('.activebtn');
+const completed = document.querySelector('.completedbtn');
+const clearcomplete = document.querySelector('.clearbtn');
 
-function addTodoDOM(e){
-    if(e.keyCode == 13){
+function showTodos(){
 
-        addTodoStorage(itemInput.value);
+    const todos = getItemsFromStorage();
 
-        const newLi = document.createElement('li');
-        newLi.classList.add('todo');
-        const text = document.createTextNode(itemInput.value);
-        // todos.style.backgroundColor='red';
-        const circle = document.createElement('div');
-        circle.classList.add('check-circle');
+    todos.forEach(todo => addTodoDOM(todo))
+}
 
-        newLi.appendChild(circle);
-        newLi.appendChild(text);
-        todos.appendChild(newLi);
+function addTodoDOM(item){
 
-        itemInput.value='';
-    }
+    const newLi = document.createElement('li');
+    newLi.classList.add('todo');
+    const text = document.createTextNode(item);
+    const circle = document.createElement('div');
+    circle.classList.add('check-circle');
+    const icon = createIcon('fa-thin fa-circle-xmark');
+    console.log(icon);
+
+    newLi.appendChild(circle);
+    newLi.appendChild(text);
+    newLi.appendChild(icon);
+    todos.appendChild(newLi);
+
+    itemInput.value='';
+}
+
+function createIcon(classes)
+{
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
 }
 
 function getItemsFromStorage(){
@@ -37,14 +53,34 @@ function getItemsFromStorage(){
     return itemsFromStorage;
 }
 
-function addTodoStorage(item){
+function addTodoStorage(e){
 
-    let itemsFromStorage = getItemsFromStorage();
-
-    itemsFromStorage.push(item);
-
-    localStorage.setItem('todos', JSON.stringify(itemsFromStorage));
+    if(e.keyCode == 13){
+        
+        let itemsFromStorage = getItemsFromStorage();
+        
+        item = itemInput.value;
+        itemsFromStorage.push(item);
+        
+        localStorage.setItem('todos', JSON.stringify(itemsFromStorage));
+        addTodoDOM(item);
+        checkUI();
+    }
 }
 
+function checkUI(){
 
-newTodo.addEventListener('keypress', addTodoDOM);
+    const todos = getItemsFromStorage();
+    if(todos.length === 0){
+        optionsBar.style.display='none';
+    }
+    else{
+        optionsBar.style.display='flex';
+    }
+}
+
+newTodo.addEventListener('keypress', addTodoStorage);
+document.addEventListener('DOMContentLoaded', showTodos);
+
+
+checkUI();
